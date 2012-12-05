5e64d8d9751aba2bfc3240b853cf18bc6f22ddf3
Albert <albeetu@gmail.com>
2012-12-05 13:40:25 -0800

    Print out members

--- group.rb
+++ group.rb
@@ -1,21 +1,31 @@
 require 'rubygems'
 require 'crack'
+require 'xmlsimple'
 require 'pp'
 
 class Group
    
-   def add_member(member)
+   @name = nil
+   @description = nil
+   @id = nil
+   @admins = Array.new
+   @members = Array.new
+
+   def add_member(member,authorized_by)
    end
  
-   def delete_member(member)
+   def delete_member(member,authorized_by)
+     # if admin, then delete
    end
 
    def import_group(config)
        #config is a well formed XML string
-       group = Crack::XML.parse(config)
-
-       @name = group['Group']['name']
-       @description = group['Group']['description']
+       group = XmlSimple.xml_in(config)
+       @name = group['name']
+       @description = group['description']
+       @id = group['id']
+       @admins = group['admin']
+       @members = group['Members']
        return group
        #error handling?
    end
@@ -30,14 +40,36 @@ class Group
 
    def list_members()
        puts "Hello #{@name}, You are all #{@description}"
-       @list['Group']['Members']['Member'].each {|k| puts "#{k.inspect}" }
-       puts "goodbye!!!!!!!"
+       puts "Members are ==========="
+       @members.each{ |mem| 
+	mem.each {|key,value| 
+                     puts value
+         }
+       }
+
    end
 
    def is_member(member)
+      #if member is in group then return true
    end
 
    def is_admin(member)
+     # if member == one of the admins, then return true
+     return @admins.include?(member)
+   end
+
+   def make_admin(member,authorized_by)
+   end
+
+   def remove_admin(member,authorized_by)
+   end
+
+   def count_members()
+   end
+
+   def list_admin()
+     puts "The admin(s) are ==============="
+     p @admins
    end
 
    def test()
--- top-down.rb
+++ top-down.rb
@@ -1,12 +1,14 @@
 require 'rubygems'
 require 'group.rb'
-require 'crack'
 
-group_config = File.read("group.xml")
 #puts group_config
 #puts Crack::XML.parse(group_config).inspect
 
 
-topdown = Group.new(nil,nil,group_config)
+topdown = Group.new(nil,nil,"group.xml")
 topdown.list_members()
+topdown.list_admin()
+topdown.is_admin("Albert Tu")
+topdown.is_admin("Mihai")
+topdown.is_admin("Hello Kitty")
 
